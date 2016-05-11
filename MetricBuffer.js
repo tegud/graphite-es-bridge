9315200381dcdc409ef1c177024b44e958fba132
Andy Lowry <andy@andylowry.me.uk>
2016-05-11 17:15:23 +0100

    Move Metric buffer into own file

---
+++ MetricBuffer.js
@@ -0,0 +1,35 @@
+const elasticsearch = require('elasticsearch');
+
+function MetricBuffer(config) {
+    let metrics = [];
+    const client = new elasticsearch.Client({
+        host: config.elasticsearch.host
+    });
+
+    setTimeout(() => {
+        const metricCount = metrics.length;
+
+        if (!metricCount) {
+            return console.log('No metrics to log.');
+        }
+
+        client.bulk({
+                body: metrics.reduce((bulkData, metric) => {
+                    bulkData.push({ index: { _index: 'metrics-2016.05', _type: 'metric' } });
+                    bulkData.push(metric);
+
+                    return bulkData;
+                }, [])
+            })
+            .then(() => console.log(`${metricCount} metrics logged to ES`))
+            .catch(err => console.log(`Error storing to es: ${err}`));
+
+        metrics = [];
+    }, config.pushEvery || 1000);
+
+    return {
+        push: metric => metrics.push(metric)
+    };
+};
+
+module.exports = MetricBuffer;
