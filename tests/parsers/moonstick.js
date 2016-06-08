c4818284bf7e9b88c864179edcffa747137d996d
Steve Elliott <steve@tegud.net>
2016-06-08 10:02:09 +0100

    Switch to stats_counts

--- tests/parsers/moonstick.js
+++ tests/parsers/moonstick.js
@@ -7,25 +7,25 @@ describe('moonstick metric parser', function() {
     it('returns nothing if the metric does not match', () =>
         should.not.exist(moonstickParser('invalid')));
 
-    describe('for matching stats', () => {
-        it('sets server', () => moonstickParser('stats.servername001.moonstick.requests_handled')
+    describe('for count stats', () => {
+        it('sets server', () => moonstickParser('stats_counts.servername001.moonstick.requests_handled')
             .should.be.have.properties({
                 server: 'servername001'
             }));
 
-        it('sets service', () => moonstickParser('stats.servername001.moonstick.requests_handled')
+        it('sets service', () => moonstickParser('stats_counts.servername001.moonstick.requests_handled')
             .should.be.have.properties({
                 service: 'moonstick'
             }));
 
-        it('sets metric', () => moonstickParser('stats.servername001.moonstick.requests_handled')
+        it('sets metric', () => moonstickParser('stats_counts.servername001.moonstick.requests_handled')
             .should.be.have.properties({
                 metric: 'requests_handled'
             }));
 
-        it('sets api when present', () => moonstickParser('stats.servername001.moonstick.hotelDetails.active_requests')
+        it('sets api when present', () => moonstickParser('stats_counts.servername001.moonstick.hotelDetails.active_requests')
             .should.be.have.properties({
                 api: 'hotelDetails'
-            }))
+            }));
     });
 });
