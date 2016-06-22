8d290e6bf6fa8cb0f00174020891a3752d8f3aa3
Steve Elliott <steve@tegud.net>
2016-06-22 22:03:29 +0100

    Status endpoint

---
+++ tests/http.js
@@ -0,0 +1,110 @@
+"use strict";
+
+const http = require('http');
+const should = require('should');
+const _ = require('lodash');
+const net = require('net');
+const FakeEsBulkServer = require('./lib/fake-es-bulk-server');
+
+const Bridge = require('../lib/server');
+
+function TestClient(ip = '127.0.0.1') {
+    const client = new net.Socket();
+
+    return {
+        start: () => new Promise(resolve => client.connect(12003, ip, () => resolve())),
+        stop: () => client.end(),
+        write: message => {
+            if(typeof message === 'string') {
+                return client.write(message);
+            }
+
+            message.forEach(part => client.write(part));
+        }
+    }
+}
+
+function makeRequest(requestOptions, body) {
+    return new Promise(resolve => {
+        var request =  http.request(_.merge({}, {
+            host: 'localhost',
+            port: 1234
+        }, requestOptions), response => {
+            var allData = '';
+
+            response.on('data', function (chunk) {
+                allData += chunk;
+            });
+
+            response.on('end', function () {
+                resolve({
+                    response: response,
+                    data: allData
+                })
+            });
+        });
+
+        if(body) {
+            request.write(body);
+        }
+
+        request.end();
+    })
+}
+
+describe('responds to http requests', function() {
+    let esServer;
+    let clients = [];
+    let bridge;
+
+    afterEach(done => Promise.all([bridge.stop(), esServer.stop(), ...clients.map(client => client.stop())]).then(() => {
+        esServer = undefined;
+        clients = [];
+        done();
+    }));
+
+    describe('status', function() {
+        it('contains active connections', () => {
+            esServer = new FakeEsBulkServer();
+            bridge = new Bridge({
+                elasticsearch: { host: '127.0.0.1:9200' },
+                pushEvery: 20
+            });
+            clients.push(new TestClient());
+
+            return Promise.all([
+                esServer.start(),
+                bridge.start()
+            ])
+            .then(() => clients[0].start())
+            .then(() => makeRequest({
+                path: '/status',
+                method: 'GET'
+            }))
+            .then(response => new Promise(resolve => resolve(JSON.parse(response.data).connections)))
+            .should.eventually.eql([ { from: '::ffff:127.0.0.1', to: 12003 } ]);
+        });
+
+        it('removes disconnected connections', () => {
+            esServer = new FakeEsBulkServer();
+            bridge = new Bridge({
+                elasticsearch: { host: '127.0.0.1:9200' },
+                pushEvery: 20
+            });
+            clients.push(new TestClient());
+
+            return Promise.all([
+                esServer.start(),
+                bridge.start()
+            ])
+            .then(() => clients[0].start())
+            .then(() => clients[0].stop())
+            .then(() => makeRequest({
+                path: '/status',
+                method: 'GET'
+            }))
+            .then(response => new Promise(resolve => resolve(JSON.parse(response.data).connections)))
+            .should.eventually.eql([]);
+        });
+    });
+});
